66918fd19dd4c819fc9afb4c75571ec3fcbf43ea
sjo <omar.idris.7e7@itb.cat>
2023-10-27 11:24:47 +0200

    sfa

--- ExercicisJavaScript/exercici1JavaScript.js
+++ ExercicisJavaScript/exercici1JavaScript.js
@@ -2,27 +2,35 @@ let numeroUsuario = window.prompt("Introdueix un número de l'1 al 100");
 let numeroRandom = Math.floor(Math.random()*100);
 
 console.log(numeroRandom);
+var cancel = false
 
-if (numeroUsuario != null) {
-    do {
+do {
+    if (numeroUsuario === null) {
+        cancel = true
+    }
+    console.log(numeroUsuario,numeroRandom)
 
-        if (numeroUsuario > numeroRandom) {
-            window.alert("El teu número és massa gran, segueix intentant.");
-            numeroUsuario = window.prompt("Introdueix un número de l'1 al 100");
-        }
-    
-        else if (numeroUsuario < numeroRandom) {
-            window.alert("El teu número és massa petit, segueix intentant.");
-            numeroUsuario = window.prompt("Introdueix un número de l'1 al 100");
-     
+    if (numeroUsuario > numeroRandom) {
+        window.alert("El teu número és massa gran, segueix intentant.");
+        numeroUsuario = window.prompt("Introdueix un número de l'1 al 100");
+    }
+
+    else if (numeroUsuario < numeroRandom) {
+        window.alert("El teu número és massa petit, segueix intentant.");
+        numeroUsuario = window.prompt("Introdueix un número de l'1 al 100");
     } 
-    }while (numeroUsuario != numeroRandom)
 
+}while (numeroUsuario != numeroRandom & !cancel)
+
+if (numeroUsuario == numeroRandom) {
     window.alert("Enhorabona, has acertat el número");
 }
-else {
+    
+if (cancel === true) {
     window.alert("Te rendiste, lamentable")
 }
+    
+
 
 
 
